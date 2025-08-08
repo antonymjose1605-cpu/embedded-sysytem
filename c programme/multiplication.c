e8be6497945b129c425d2282d3729d3a519838e0
Antony <antony.m.jose1605@gmail.com>
2025-08-08 17:12:38 +0530

    multiplication done

---
+++ c programme/multiplication.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int multi(int,int);
+
+int main()
+{
+    int a, b, ret_sum;
+
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    ret_sum = multi(a, b);
+
+    printf("The difference is: %d\n", ret_sum);
+
+    return 0;
+}
+int multi(int a, int b)
+{
+    int sum= a * b;
+    return sum;
+}
